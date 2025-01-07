d4ac7b1dc1c25d9bda4b70a1446314d289fa8941
Serhatuzn <seerhatuzun@gmail.com>
2025-01-07 18:26:54 +0300

    LibaryMenagment

--- LibaryMenagment/obj/Debug/net9.0/MvcProject.AssemblyInfo.cs
+++ LibaryMenagment/obj/Debug/net9.0/MvcProject.AssemblyInfo.cs
@@ -12,9 +12,9 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcProject")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c25f140736b5ccd49204a2cde353393d621e1745")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09c9260385bb558fbc966b512eafc9e90e19a0d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
